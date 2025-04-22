cd966df1bcd869f3e69204b86dfa09d628f5f74d
ASD-9 <ryan.chavatte@gmail.com>
2025-04-22 17:11:29 +0200

    database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT) ?? 3306,
+      username: process.env.DB_USER ?? 'root',
+      password: process.env.DB_PSWD ?? 'root',
+      database: process.env.DB_NAME ?? 'eiga',
+      autoLoadEntities: true,
+      synchronize: false,
+      extra: {
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
