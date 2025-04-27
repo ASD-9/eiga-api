7c134ea6ed27bf7dd854be0aedaa71eeb9ede780
ASD-9 <ryan.chavatte@gmail.com>
2025-04-27 21:54:10 +0200

    add findOneById function and export CategoriesService

--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -32,6 +32,7 @@ describe('CategoriesService', () => {
           useValue: {
             save: jest.fn(),
             find: jest.fn(),
+            findOneBy: jest.fn(),
             update: jest.fn(),
             delete: jest.fn(),
           },
@@ -73,6 +74,33 @@ describe('CategoriesService', () => {
       jest.spyOn(repository, 'find').mockRejectedValue(new Error('Error'));
 
       await expect(service.findAll()).rejects.toThrow(
+        new InternalServerErrorException('Erreur serveur, veuillez réessayer'),
+      );
+    });
+  });
+
+  describe('findOneById', () => {
+    it('should return the category with the given id', async () => {
+      const id = 1;
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(mockData);
+
+      expect(await service.findOneById(id)).toEqual(mockData);
+    });
+
+    it('should thrown NotFoundException if the category is not found', async () => {
+      const id = 99;
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(null);
+
+      await expect(service.findOneById(id)).rejects.toThrow(
+        new NotFoundException(`Catégorie ${id} introuvable`),
+      );
+    });
+
+    it("should throw InternalServerErrorException if there's an error", async () => {
+      const id = 1;
+      jest.spyOn(repository, 'findOneBy').mockRejectedValue(new Error('Error'));
+
+      await expect(service.findOneById(id)).rejects.toThrow(
         new InternalServerErrorException('Erreur serveur, veuillez réessayer'),
       );
     });
